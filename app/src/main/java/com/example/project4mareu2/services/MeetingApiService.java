3dad266b496d27c3f5addc2ae7c03c9494e9c18b
Maxime <mfitzjean@gmail.com>
2020-06-03 09:07:28 +0200

    Filter Api

--- app/src/main/java/com/example/project4mareu2/services/MeetingApiService.java
+++ app/src/main/java/com/example/project4mareu2/services/MeetingApiService.java
@@ -4,6 +4,7 @@ import com.example.project4mareu2.models.Meeting;
 import com.example.project4mareu2.models.Participants;
 
 import java.util.List;
+import java.util.logging.Filter;
 
 public interface MeetingApiService {
 
@@ -14,5 +15,9 @@ public interface MeetingApiService {
     void deleteMeetings(Meeting meeting);
 
     List<Participants> getMeetingParticipants();
+
+    Filter getFilter();
+
+    Filter getDateFilter();
 
 }
--- app/src/main/java/com/example/project4mareu2/services/MeetingListApiService.java
+++ app/src/main/java/com/example/project4mareu2/services/MeetingListApiService.java
@@ -5,10 +5,12 @@ import com.example.project4mareu2.models.Participants;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Filter;
 
 public class MeetingListApiService implements MeetingApiService {
 
     private List<Meeting> meetings = MeetingGenerator.generateMeeting();
+    private Filter dateFilter = MeetingGenerator.generateDateFilter();
 
     public List<Meeting> getMeetings() {
         return new ArrayList<>(meetings);
@@ -28,6 +30,16 @@ public class MeetingListApiService implements MeetingApiService {
     @Override
     public List<Participants> getMeetingParticipants() {
         return mParticipants;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return dateFilter;
+    }
+
+    @Override
+    public Filter getDateFilter() {
+        return null;
     }
 
     private List<Participants> mParticipants = MeetingGenerator.generateParticipants();
